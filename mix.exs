fde3734d61caebad84396dba5d92ba4dcda010e2
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-02-11 12:00:16 +0000

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Dry.MixProject do
   def project do
     [
       app: :dry,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
